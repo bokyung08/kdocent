8c9bbb040f9dcac794f8d3e0ac5ebf147ee61ed6
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-08-20 15:16:57 +0900

    작품 리스트 수정

--- src/components/BurgerButton.jsx
+++ src/components/BurgerButton.jsx
@@ -33,15 +33,24 @@ function Popup({ isOpen, onClose }) {
         className={`popup-content ${isOpen ? 'open' : ''}`}
         onClick={(e) => e.stopPropagation()}
       >
-        <h3>❖ 미술관 지도</h3>
+        <h2>❖ 작품 리스트 ❖</h2>
+        {/* --- */}
         <hr />
         <ul>
-          <li><h4>Part 1. Self-Portraits</h4></li>
-          <li><h4>Part 2. Landscapes</h4></li>
-          <li><h4>Part 3. Flowers</h4></li>
+          <h3>세션 1. "현실과 내면의 탐구"</h3>
+            <li>- 감자먹는 사람들</li>
+            <li>- 자화상 1887</li>
+          <h3>세션 2. "빛과 생명: 해바라기와 자화상"</h3>
+            <li>- 해바라기</li>
+            <li>- 자화상 1889</li>
+	        <h3>세션 3. "고독 속의 희망과 새로운 시작"</h3>
+            <li>- 별이 빛나는 밤</li>
+            <li>- 꽃 피는 아몬드 나무</li> 
         </ul>
         <hr />
-        <h4>☻ 고객센터</h4>
+        {/* --- */}
+        <h3><u>☻ 미술관 지도</u></h3>
+        <h3><u>☻ 고객센터</u></h3>
       </div>
     </div>
   );
